a1e40bf70f2eb4f21e08119e86d88e91cfb2fdf3
Rishabh More <rushabh.more@wednesday.is>
2024-02-12 11:24:36 +0530

    Fix imports

--- app/src/main/java/com/midnight/danger/cellcascades/MainActivity.kt
+++ app/src/main/java/com/midnight/danger/cellcascades/MainActivity.kt
@@ -3,15 +3,10 @@ package com.midnight.danger.cellcascades
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.midnight.danger.cellcascades.ui.theme.CellCascadesTheme
 import com.midnight.danger.neumorphicdesignsystem.components.surface.NDSScaffold
 import com.midnight.danger.neumorphicdesignsystem.theme.NeumorphicDesignTheme
 
